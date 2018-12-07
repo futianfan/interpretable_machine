feafc2d62638cf758452e507f9f69d05bada1c73
futianfan <futianfan@gmail.com>
2018-12-07 08:30:24 -0500

    wrap

--- README.md
+++ README.md
@@ -5,10 +5,14 @@
 	@property: read-only attribute
 	@staticmethod: no "self";   static method in class, no "self"
 	@classmethod: no "self"; self is replace by "class"
+
 3. args_kwargs.py:   *args && **kwargs
 	args
 	**kwargs  dictionary
 		config file dict = 'src_size':10, 'trg_size':20
 		**dict
 		
+4.  decorator/wrap:
+	decorator with parameter
+
 
--- wrapp.py
+++ wrapp.py
@@ -1,3 +1,42 @@
+from time import time
 
+
+
+def decorator1(func):
+	def wrapper1():
+		t1 = time()
+		func()
+		t2 = time()
+		print('Running %s takes %s seconds' % (func.__name__, str(t2 - t1)))
+	return wrapper1
+
+@decorator1
+def f1():
+	print('test inside f1')
+
+
+
+class decorator2:
+	def __init__(self, func):
+		self._func = func 
+		print('flag1************')
+
+	def __call__(self):
+		print('flag2')
+		t1 = time()
+		self._func()
+		t2 = time()
+		print('Running %s cost %s seconds ' % (self._func.__name__, str(t2-t1)) )
+
+@decorator2
+def f2():
+	print('test inside %s' % 'f2')
+
+
+
+
+if __name__ == '__main__':
+	f1()
+	#f2()
 
 
